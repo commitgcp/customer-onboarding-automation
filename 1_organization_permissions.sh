2b5e3623a05f0c26a4023bfde440c9cffe65ad76
Akiva Ashkenazi <akiva.ashkenazi@comm-it.cloud>
2024-04-01 16:35:51 +0300

    Added organization permission script

---
+++ 1_organization_permissions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prompt the user for their organization ID
+read -p "Please enter your organization ID: " org_id
+
+# Check if the organization ID input is blank
+if [ -z "$org_id" ]; then
+    echo "You need an organization ID in order to continue."
+    echo "Please refer to the tutorial on the right side of the page if you require assistance."
+else
+    # Assign Viewer role to the principal
+    gcloud organizations add-iam-policy-binding "$org_id" \
+        --member="user:gcp-support@comm-it.cloud" \
+        --role="roles/viewer"
+
+    # Assign Browser role to the principal
+    gcloud organizations add-iam-policy-binding "$org_id" \
+        --member="user:gcp-support@comm-it.cloud" \
+        --role="roles/browser"
+
+    echo "Viewer and Browser IAM roles have been assigned to gcp-support@comm-it.cloud for organization ID $org_id"
+fi
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ development environment managing resources hosted on Google Cloud Platform.
 
 This repository contains an automated onboarding process for Commit-GCP customers.
 
-Click the open in Cloud Shell button below to begin.
+Click the open in Cloud Shell button below and select "Trust repo" and "confirm" to begin. Instructions will appear on the right side of the screen.
 
 [![Open this project in Cloud
 Shell](http://gstatic.com/cloudssh/images/open-btn.png)](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/commitgcp/onboarding-automation.git&page=editor&tutorial=tutorial.md)
